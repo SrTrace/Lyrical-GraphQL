a44510210c29955985ab58b01a0f813da6c49978
Sergey <levashov04sergey@gmail.com>
2021-10-13 20:29:52 +0300

    restart after crashim webpack

--- client/index.js
+++ client/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient} from "apollo-client";
+import {ApolloProvider} from "react-apollo";
+import {Router, Route, hashHistory, IndexRoute} from "react-router";
+import { createBrowserHistory } from "history";
+
+//import { InMemoryCache } from 'apollo-cache-inmemory';
+//import { HttpLink } from 'apollo-link-http';
+
+
+import App from './components/App';
+import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
+
+const client = new ApolloClient({});
+//const cache = new InMemoryCache();
+//const history = createBrowserHistory();
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+      <ApolloProvider client={client}>
+          <Router history={hashHistory}>
+              <Route path="/" component={App}>
+                  <IndexRoute component={SongList}/>
+                  <Route path="song/new" component={SongCreate}/>
+              </Route>
+          </Router>
+      </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
