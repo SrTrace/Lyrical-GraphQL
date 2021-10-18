21b404dc6bfc31a614c2ee09ba7e6ee0dd2eb902
Sergey <levashov04sergey@gmail.com>
2021-10-18 22:50:49 +0300

    adding mutation delete and add for  SongList, SongCreate

--- client/index.js
+++ client/index.js
@@ -1,21 +1,16 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ApolloClient} from "apollo-client";
 import {ApolloProvider} from "react-apollo";
 import {Router, Route, hashHistory, IndexRoute} from "react-router";
-import { createBrowserHistory } from "history";
-
-//import { InMemoryCache } from 'apollo-cache-inmemory';
-//import { HttpLink } from 'apollo-link-http';
-
 
 import App from './components/App';
 import SongList from "./components/SongList";
 import SongCreate from "./components/SongCreate";
 
 const client = new ApolloClient({});
-//const cache = new InMemoryCache();
-//const history = createBrowserHistory();
+
 
 const Root = () => {
   return (
@@ -23,7 +18,7 @@ const Root = () => {
           <Router history={hashHistory}>
               <Route path="/" component={App}>
                   <IndexRoute component={SongList}/>
-                  <Route path="song/new" component={SongCreate}/>
+                  <Route path="songs/new" component={SongCreate}/>
               </Route>
           </Router>
       </ApolloProvider>
